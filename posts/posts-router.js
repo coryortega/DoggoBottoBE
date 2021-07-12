0fd33c425d958455fa9334e01d0ed2bd0cc96eb5
Cory Ortega <coryortega@Corys-MBP.home>
2021-07-12 16:03:05 -0700

    removed typo, added status

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -10,9 +10,9 @@ const db = require("../database/dbConfig");
 //router.options("/images")
 router.options("/images", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.send(200);
+  res.status(200);
 });
 
 router.get("/images", (req, res) => {

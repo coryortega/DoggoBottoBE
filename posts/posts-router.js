38ba658f99ba721ab729ff216116038c1d5865b7
Cory Ortega <coryortega@Corys-MBP.home>
2021-07-12 15:59:48 -0700

    configured options

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -7,7 +7,13 @@ const upload = require("../services/file-upload");
 
 const db = require("../database/dbConfig");
 
-router.options("/images")
+//router.options("/images")
+router.options("/images", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
 
 router.get("/images", (req, res) => {
   Posts.find()
